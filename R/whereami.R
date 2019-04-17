3ebf1fc6b71ddec99dea26cff0d82fb92c968bc8
Jonathan Sidi <yonis@metrumrg.com>
2019-04-16 21:30:06 -0400

    add print method and cat_whereami

--- R/whereami.R
+++ R/whereami.R
@@ -74,6 +74,31 @@ whereami <- function(path_expand = FALSE){
     return(readLines(tf))
   }
 
-  readLines(tf)
+  ret <- readLines(tf)
+
+  structure(ret,class = c('whereami'))
+
+}
+
+#' @export
+print.whereami <- function(x){
+  if(length(x)==1)
+    return(cli::cat_rule(sprintf("Running From: %s",x)))
+
+  place <- basename(x[length(x)])
+  fun   <- deparse(sys.calls()[[sys.nframe() - 1]])
+
+  cli::cat_rule(sprintf("Running %s at %s",paste0(fun,collapse = ', '),place))
+
+}
+
+#' @export
+cat_where <- function(where){
+  if(length(where)==1)
+    return(where)
+
+  place = basename(where[length(where)])
+  fun = deparse(sys.calls()[[sys.nframe() - 1]])
+  cli::cat_rule(sprintf("Running %s at %s",fun,place))
 
 }
