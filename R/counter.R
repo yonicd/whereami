6de50347c0f499560d8f85dc3a00201e65726724
Jonathan Sidi <yonis@metrumrg.com>
2019-04-19 09:58:02 -0400

    add: counter_wrapper to safely access internal env

--- R/counter.R
+++ R/counter.R
@@ -1,12 +1,34 @@
 wenv <- new.env()
 
-
-#' @title Reset the internal counter
-#' @description Reset the internal counter for whereami to an empty list
-#' @return NULL
-#' @rdname reset_whereami
+#' @title whereami counter
+#' @description Interact with internal whereami counter
+#' @details
+#' counter_state returns current hit count for each stored counter,
+#' arranged in order of counter initialization.
+#'
+#' counter_reset will remove all stored counters.
+#'
+#' @rdname counter
 #' @export
-reset_whereami <- function(){
+counter_state <- function(){
+
+  empty_msg <- 'No active counters'
+
+  e <- wenv
+
+  if(!hasName(x = e,'counter'))
+    return(empty_msg)
+
+  if(!length(e$counter))
+    return(empty_msg)
+
+  as.numeric(e$counter)
+
+}
+
+#' @rdname counter
+#' @export
+counter_reset <- function(){
   assign(x = 'counter',value = list(),envir = wenv)
 }
 
