70837ad06cf6bcc4085caa7f0a41373ca8e6951e
Jonathan Sidi <yonis@metrumrg.com>
2019-04-21 08:24:33 -0400

    tests: add more tests for multiple counters

--- tests/testthat/test-whereami.R
+++ tests/testthat/test-whereami.R
@@ -10,7 +10,7 @@ testthat::describe("counter flow", {
   skip_if_not_rstudio()
 
   it('initial',{
-    testthat::expect_output(source('foo.R'),'foo.R#4 \\(1\\)')
+    testthat::expect_output(source('print.R'),'print.R#4 \\(1\\)')
   })
 
   it('check counter was created',{
@@ -18,19 +18,36 @@ testthat::describe("counter flow", {
   })
 
   it('counter name',{
-    testthat::expect_equal(counter_names(),'foo.R#4')
+    testthat::expect_equal(counter_names(),'print.R#4')
   })
 
   it('counter bump',{
-    testthat::expect_output(source('foo.R'),'foo.R#4 \\(2\\)')
+    testthat::expect_output(source('print.R'),'print.R#4 \\(2\\)')
   })
 
   it('counter state',{
-    testthat::expect_equal(counter_state(),list('foo.R#4' = 2))
+    testthat::expect_equal(counter_state(),list('print.R#4' = 2))
   })
 
-  it('reset',{
-    counter_reset('foo.R#4')
+  it('cat_whereami',{
+    testthat::expect_output(source('cat.R'),'cat.R#3 \\(1\\)')
+  })
+
+  it('multiple counter states',{
+    testthat::expect_equal(counter_state(),list('print.R#4' = 2,'cat.R#3' = 1))
+  })
+
+  it('multiple counter names',{
+    testthat::expect_equal(counter_names(),c('print.R#4','cat.R#3'))
+  })
+
+  it('reset item',{
+    counter_reset('print.R#4')
+    testthat::expect_null(counter_state('print.R#4'))
+  })
+
+  it('reset all',{
+    counter_reset()
     testthat::expect_false(counter_check())
   })
 
